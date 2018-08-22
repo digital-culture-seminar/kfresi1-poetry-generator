04bdf14e7f116fcbbc8c67d2bb0554f6b3efac6e
kfresi1 <kfresi1@lsu.edu>
2018-08-21 20:49:07 -0500

    first python script :snake:

---
+++ hello world.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tus Aug 21 
+@author: Katherine 
+License: CC-BY-SA 4.0
+Description: a simple python script
+This is a temporary script file.
+"""
+
+# set variables
+world_adjective = "new"
+
+# print statement 
+print "hello {adj} world!".format(adj=world_adjective)
+
+
+
+ 
