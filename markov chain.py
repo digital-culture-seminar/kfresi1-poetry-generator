aefb72d4da6d643ccd99fc5cb5ee2dbff75cad99
kfresi1 <kfresi1@lsu.edu>
2018-10-09 12:00:22 -0500

    the raven added
    the raven added

--- markov chain.py
+++ markov chain.py
@@ -9,28 +9,48 @@ markov chain
 import markovify
 
 
-with open ("alice.txt") as textfile: 
-    carroll = textfile.read()
-    
-with open ("poe.txt") as textfile: 
-    poe = textfile.read()
-    
-#print carroll
-
-carroll_model = markovify.Text(carroll)  
-poe_model = markovify.Text(poe) 
-
-synthesized_model = markovify.combine([carroll_model, poe_model], [1,1])
-
-print synthesized_model.make_sentence()
+#with open ("alice.txt") as textfile: 
+#    carroll = textfile.read()
+#    
+#with open ("poe.txt") as textfile: 
+#    poe = textfile.read()
+#    
+##print carroll
+#
+#carroll_model = markovify.Text(carroll)  
+#poe_model = markovify.Text(poe) 
+#
+#synthesized_model = markovify.combine([carroll_model, poe_model], [1,1])
+#
+#print synthesized_model.make_sentence()
 
 
 #print carroll_model.make_sentence()
 
 
-##print carroll_model.make_short_sentence(50)
+#print carroll_model.make_short_sentence(50)
 
-for i in range(3):
-    print carroll_model.make_sentence()
-    print " "
+#for i in range(3):
+#    print carroll_model.make_sentence()
+#    print " "
     
+with open ("poe.txt") as textfile: 
+    poe = textfile.read()
+
+with open ("the raven.txt") as textfile: 
+    raven = textfile.read()
+    
+#print raven
+
+raven_model = markovify.Text(raven)  
+poe_model = markovify.Text(poe) 
+
+synthesized_model = markovify.combine([raven_model, poe_model], [1,1])
+
+print synthesized_model.make_sentence()
+#
+#
+#print raven_model.make_sentence()
+#
+#
+##print raven_model.make_short_sentence(10)
