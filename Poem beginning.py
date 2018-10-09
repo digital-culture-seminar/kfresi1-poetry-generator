c9912333f86de871723eecffa8e2a9421f35acdf
kfresi1 <kfresi1@lsu.edu>
2018-10-09 12:38:03 -0500

    added markov chain
    added markov chain

--- Poem beginning.py
+++ Poem beginning.py
@@ -74,6 +74,7 @@ print "while the {sec_adj} {sec_n} {sec_adv} {sec_v} through viridian leaves.".f
 #    i=i
 #   
 #add random if else to my poem 
+random.seed()
 path = random.randint(1,3)
 path = path
 
@@ -86,24 +87,40 @@ if path == 2:
 else:
     print "The wander then comes upon a clearing."
 
+random.seed()
+
 print "The {third_adj} {third_n} {third_adv} {third_v}.".format(third_adj=adjective, third_n=third_noun, third_adv=third_adverb, third_v=third_verb)
-##add markovify to my poem
-#import markovify
-#
-#
-#with open ("alice.txt") as textfile: 
-#    carroll = textfile.read()
-#    
+#add markovify to my poem
+import markovify
+
+
+with open ("alice.txt") as textfile: 
+    carroll = textfile.read()
+    
+with open ("poe.txt") as textfile: 
+    poe = textfile.read()
+    
+#print carroll
+
+carroll_model = markovify.Text(carroll)  
+poe_model = markovify.Text(poe) 
+
+synthesized_model = markovify.combine([carroll_model, poe_model], [1,1])
+
+print synthesized_model.make_sentence()
+
+
 #with open ("poe.txt") as textfile: 
 #    poe = textfile.read()
-#    
-##print carroll
 #
-#carroll_model = markovify.Text(carroll)  
+#with open ("the raven.txt") as textfile: 
+#    raven = textfile.read()
+#    
+##print raven
+#
+#raven_model = markovify.Text(raven)  
 #poe_model = markovify.Text(poe) 
 #
-#synthesized_model = markovify.combine([carroll_model, poe_model], [1,1])
+#synthesized_model = markovify.combine([raven_model, poe_model], [1,1])
 #
 #print synthesized_model.make_sentence()
-
-
